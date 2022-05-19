327f9469c5cea03cb611e075540eace8f54b2d72
shaolin055 <shafiqul.eee.buet@gmail.com>
2022-05-19 11:58:48 -0400

    Delete Constraction site currently signed on directory

--- Constraction site currently signed on/front2.cpp
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-main()
-{	printf("\n\n\n\n\n1\t\t\t ....\n\t\t\t\b\b\b.\t\t.\n\t\t\t\b\b\b\b\b.\t\t   .\n\t\t\t\b\b\b\b\b\b\b.\t\t    .\n\t\t.\t\t     .\n\t\t.\t\t     .");
-	printf("\n2\t\t.\t\t     .\n\t\t .\t\t    .");
-	printf("\n3\t\t  .\t\t   .\n\t\t    .\t\t  .\n\t\t       .\t.\n\t\t\t  ...");
-}
